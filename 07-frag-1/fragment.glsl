b71da5882ec96f020ae8abdce61c158e78407d0d
romxz <andreiromalv@gmail.com>
2019-11-26 23:02:59 -0500

    out: fragment shaders in: vertex shaders

--- 07-frag-1/fragment.glsl
+++ 07-frag-1/fragment.glsl
@@ -7,7 +7,8 @@ void main() {
 
   //TODO: Replace this with a function that draws a circle at (256.5,256.5) with radius 128
   
-  if(gl_FragCoord.y > 256.0) {
+  //if(gl_FragCoord.y > 256.0) {
+  if(length(gl_FragCoord.xy - vec2(256.0,256.0)) <= 128.0) {
     gl_FragColor = CIRCLE_COLOR;
   } else {
     gl_FragColor = OUTSIDE_COLOR;
--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -7,6 +7,7 @@ void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
 
   //TODO: Swap red and blue color channels of image
-
+  
   gl_FragColor = texture2D(texture, coord);
+  gl_FragColor.rb = gl_FragColor.br;
 }
