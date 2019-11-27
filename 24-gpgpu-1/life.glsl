2f3096f3591621fc57af655a2fa4e5fa4420ffa5
romxz <andreiromalv@gmail.com>
2019-11-27 18:32:26 -0500

    out: gpgpu: conway's game of life. Bug: shader works but button to get [COMPLETE] doesn't work

--- 24-gpgpu-1/life.glsl
+++ 24-gpgpu-1/life.glsl
@@ -12,7 +12,19 @@ void main() {
 
 
   //TODO: Compute the next state for the cell at coord
+  //float s_d = state(coord);
   float s = state(coord);
-
+  float numAlive = 0.0;
+  
+  for (int i = -1; i <= 1; i++) {
+    for (int j = -1; j <= 1; j++) {
+	  vec2 neighborCoord = fract((coord + vec2(i,j)) / stateSize);// + fract();
+	  numAlive += texture2D(prevState, neighborCoord).r;
+    }
+  }
+  
+  s = numAlive > 3.0+s || numAlive < 3.0 ? 0.0 : 1.0;
   gl_FragColor = vec4(s,s,s, 1.0);
+  //gl_FragColor = vec4(s_d,s_d,s_d, 1.0);
+  //gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
