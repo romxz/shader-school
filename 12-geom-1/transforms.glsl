d66523a9e1aee14e685193dee2ad7cfda89c3b4e
romxz <andreiromalv@gmail.com>
2019-11-27 01:01:37 -0500

    out: geometry in: lighting

--- 12-geom-1/transforms.glsl
+++ 12-geom-1/transforms.glsl
@@ -7,6 +7,8 @@ uniform mat4 model, view, projection;
 void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
-
-  gl_Position = vec4(position, 1);
+  gl_Position = projection * view * model * vec4(position, 1);
+  //gl_Position = view * model * vec4(position, 1);
+  //gl_Position = model * vec4(position, 1);
+  //gl_Position = vec4(position, 1);
 }
--- 16-geom-5/rotate.glsl
+++ 16-geom-5/rotate.glsl
@@ -1,11 +1,16 @@
 highp mat4 rotation(highp vec3 n, highp float theta) {
 
   //TODO: Using Rodrigues' formula, find a matrix which performs a rotation about the axis n by theta radians
-
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  float c = cos(theta);
+  float s = sin(theta);
+  
+  highp vec3 x = vec3(1.0,0,0);
+  highp vec3 y = vec3(0,1.0,0);
+  highp vec3 z = vec3(0,0,1.0);
+  return mat4(x * c + cross(n,x) * s + n * dot(x,n) * (1.0-c), 0, 
+			  y * c + cross(n,y) * s + n * dot(y,n) * (1.0-c), 0,
+              z * c + cross(n,z) * s + n * dot(z,n) * (1.0-c), 0,
+			  0, 0, 0, 1);
 }
 
 #pragma glslify: export(rotation)
